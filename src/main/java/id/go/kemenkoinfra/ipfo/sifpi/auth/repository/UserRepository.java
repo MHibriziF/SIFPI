6e7d953d112e2050760e94274fe2eb07d5654daa
Theo Ananda Lemuel <theo.ananda@ui.ac.id>
2026-03-06 21:16:49 +0700

    feat( user ): implement bulk insert user

--- src/main/java/id/go/kemenkoinfra/ipfo/sifpi/auth/repository/UserRepository.java
+++ src/main/java/id/go/kemenkoinfra/ipfo/sifpi/auth/repository/UserRepository.java
@@ -1,11 +1,15 @@
 package id.go.kemenkoinfra.ipfo.sifpi.auth.repository;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import id.go.kemenkoinfra.ipfo.sifpi.auth.model.User;
 
@@ -17,6 +21,9 @@ public interface UserRepository extends JpaRepository<User, UUID> {
     Page<User> findAll(Pageable pageable);
 
     boolean existsByEmail(String email);
+
+    @Query("select lower(u.email) from User u where lower(u.email) in :emails")
+    List<String> findExistingEmailsIgnoreCase(@Param("emails") Collection<String> emails);
 
     // For password management
     Optional<User> findByPasswordSetupToken(String token);
